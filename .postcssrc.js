d770a4e5ad252ea35a2542c39a9c0ff81af473b8
Даниил Пронин <mail@grawl.ru>
2021-12-07 18:24:05 +1000

    Initial commit

---
+++ .postcssrc.js
@@ -0,0 +1,10 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+module.exports = {
+  // parser: 'sugarss',
+  plugins: [
+    // require('precss'),
+    // to edit target browsers: use "browserslist" field in package.json
+    require('autoprefixer')
+  ]
+}
